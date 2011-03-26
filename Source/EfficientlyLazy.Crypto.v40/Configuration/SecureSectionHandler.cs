7415b821cfa15e29cbee92672f821ce3c51b2f63
Jason LaFlair <jason@laflair.net>
2011-03-26 04:18:14 +0000

    Added Licensing headers to source files

--- Source/EfficientlyLazy.Crypto.v40/Configuration/SecureSectionHandler.cs
+++ Source/EfficientlyLazy.Crypto.v40/Configuration/SecureSectionHandler.cs
@@ -1,10 +1,24 @@
-﻿using System;
-using System.Configuration;
-using System.Xml;
-using System.Xml.Serialization;
-
+﻿// // Copyright 2008-2009 LaFlair.NET
+// // 
+// // Licensed under the Apache License, Version 2.0 (the "License");
+// // you may not use this file except in compliance with the License.
+// // You may obtain a copy of the License at
+// // 
+// //     http://www.apache.org/licenses/LICENSE-2.0
+// // 
+// // Unless required by applicable law or agreed to in writing, software
+// // distributed under the License is distributed on an "AS IS" BASIS,
+// // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// // See the License for the specific language governing permissions and
+// // limitations under the License.
+// 
 namespace EfficientlyLazy.Crypto.Configuration
 {
+    using System;
+    using System.Configuration;
+    using System.Xml;
+    using System.Xml.Serialization;
+
     ///<summary>
     /// Handles access to secured configuration sections.
     ///</summary>
